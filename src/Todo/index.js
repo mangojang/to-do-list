da7fa521c31e62c8f561a6157c7215d5fd326c93
mangojang994 <mangojang994@gmail.com>
2023-04-13 16:55:42 +0900

    Add : Todo create

--- src/Todo/index.js
+++ src/Todo/index.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AppLayout from "../AppLayout";
 import TodoInput from "../TodoInput";
@@ -6,21 +6,30 @@ import TodoList from "../TodoList";
 
 const Todo = ()=>{
     const navigate = useNavigate();
+    const [token, setToken] = useState('');
 
 
     useEffect(()=>{
-        const token = localStorage.getItem('todo');
-        if(!token){
+        const accessToken = localStorage.getItem('todo');
+        if(accessToken){
+            setToken(accessToken);
+        }else{
             navigate('/signin', {replace: true});
         }
     },[])
+    
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
 
     return(
         <AppLayout type={"todo"}>
             <div className="inner">
                 <h1>To Do List</h1>
                 <div className="box_top">
-                    <TodoInput/>
+                    <TodoInput config={config}/>
                 </div>
                 <div className="box_bottom">
                     <TodoList/>
--- src/TodoInput/index.js
+++ src/TodoInput/index.js
@@ -1,12 +1,42 @@
+import axios from "axios";
+import { useCallback, useState } from "react";
+import { backURL } from "../config";
 import { Button, Input } from "../Styles";
 import { Conatainer, Row } from "./style";
 
-const TodoInput = ()=>{
+const TodoInput = (props)=>{
+    const [todo, setTodo] = useState('');
+    console.log('props',props);
+
+    const onChangeTodo = useCallback((e)=>{
+        setTodo(e.target.value);
+    },[])
+    
+    const onSubmit = useCallback((e)=>{
+        e.preventDefault();
+        const data = {
+            todo
+        }
+        
+
+        axios.post(`${backURL}/todos`, data, props.config)
+        .then(response=>{
+            console.log('성공',response.data);
+            //1. setTodo('');
+            setTodo('');
+            //2. 받은 값 state 추가  -> 상위 컴포넌트에서 함수 끌어다 쓰기
+        })
+        .catch(error=>{
+            console.log('에러', error);
+            alert('잠시후 다시 시도해 주세요.')
+        })
+    },[todo])
+
     return(
         <Conatainer>
-            <form>
+            <form onSubmit={onSubmit}>
                 <Row>
-                    <Input type={'text'} id='todo' data-testid="new-todo-input" />
+                    <Input type={'text'} id='todo' data-testid="new-todo-input" value={todo} onChange={onChangeTodo} />
                     <Button type="submit" styletype="small" data-testid="new-todo-add-button">추가</Button>
                 </Row>
             </form>
